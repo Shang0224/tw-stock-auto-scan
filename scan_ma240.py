32bd9ce85887f849cfecf88dcf75d3f81cee4353
Shang0224 <shangkuo0224@gmail.com>
2026-04-02 13:22:29 +0800

    Update scan_ma240.py

--- scan_ma240.py
+++ scan_ma240.py
@@ -4,54 +4,13 @@ import time
 from datetime import datetime, timedelta, timezone
 import os
 import requests
+from untls import send_line_message
 
 from strategy.near_ma240 import st_near_ma240, st_near_ma240_df
 from strategy.advanced_ma240 import st_advanced_ma240, st_advanced_ma240_df
 from scanstock.scanstock import scan_stocks_df, scan_stocks_df_list
 
-def smart_read_csv(file_path):
-    # 測試清單：UTF-8 (現代標準), Big5 (台灣常見), UTF-8-SIG (Excel 專用)
-    encodings = ['utf-8', 'big5', 'utf-8-sig', 'cp950']
-    
-    for enc in encodings:
-        try:
-            df = pd.read_csv(file_path, encoding=enc)
-            print(f"✅ 成功使用 {enc} 編碼讀取檔案！")
-            return df
-        except UnicodeDecodeError:
-            continue
-    
-    print("❌ 找不到匹配的編碼，請檢查檔案格式。")
-    return None
 
-def send_line_message(message):
-    """透過 LINE Messaging API 發送訊息"""
-    token = os.getenv("LINE_ACCESS_TOKEN")
-    user_id = os.getenv("LINE_USER_ID")
-
-    print(f"USER ID : {user_id}\nMessage {message}")
-    
-    if not token or not user_id:
-        print("錯誤：找不到 LINE 的設定資訊 (Secrets)")
-        return
-
-    url = "https://api.line.me/v2/bot/message/push"       
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {token}"
-    }
-    payload = {
-        "to": user_id,
-        "messages": [{"type": "text", "text": message}]
-    }
-
-    print(f"USER ID : {user_id}\nMessage {message}")
-    
-    res = requests.post(url, headers=headers, json=payload)
-    if res.status_code == 200:
-        print("LINE 報告發送成功！")
-    else:
-        print(f"發送失敗，狀態碼：{res.status_code}, 內容：{res.text}")
 
 
 def fetch_all_stocks(dl, stock_ids, start_date, end_date):
