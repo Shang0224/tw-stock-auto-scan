db05881821929fb1f3b51b4a1295f1cc3d76163e
Shang0224 <shangkuo0224@gmail.com>
2026-04-21 10:30:54 +0800

    Update scan_ma240.py

--- scan_ma240.py
+++ scan_ma240.py
@@ -416,7 +416,7 @@ if __name__ == "__main__":
     # 邏輯判定：只有在「定時排程」時才切換到今日時間
     if event_name == 'schedule':    
         finmind_scan_ma240()
-    elif data_testing
+    elif data_testing :
         test_data()
     else:
         yfinance_scan_ma240()
