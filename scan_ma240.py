ff958de2af17e661b06702316549eaba5e4e45cc
Shang0224 <shangkuo0224@gmail.com>
2026-07-03 14:43:49 +0800

    Update scan_ma240.py

--- scan_ma240.py
+++ scan_ma240.py
@@ -11,7 +11,7 @@ from strategy.near_ma240 import st_near_ma240, st_near_ma240_df
 from scanstock.scanstock import scan_stocks_df, scan_stocks_df_list
 from strategy.advanced_ma240 import st_advanced_ma240, st_advanced_ma240_df, st_advanced_ma240_df_up
 from strategy.bottom_rebound import st_bottom_v_turn, st_bottom_breakout, st_bottom_consolidation
-
+from strategy.strat_right_side import ‎st_bottom_u_turn
 
 # 宣告控制測試資料的開關 (True: 執行測試資料 / False: 執行正式資料)
 RUN_TEST_DATA = False
