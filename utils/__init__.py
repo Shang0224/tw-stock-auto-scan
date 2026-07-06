0e62056fd5dc4267845e5244630527404497d971
Shang0224 <shangkuo0224@gmail.com>
2026-07-06 13:41:49 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,7 +4,7 @@
 from utils.data_fetcher import yf_fetch_all_stocks, fm_fetch_all_stocks
 from utils.notifier import send_line_message, send_email_with_csv
 from utils.storage import upload_to_nas
-from utils.file_handler import parse_stock_ids, get_stock_name_dict, send_report, cleanup_local_file
+from utils.file_handler import parse_stock_ids, get_stock_name_dict, send_report, cleanup_local_file, 'archive_and_cleanup'
 
 # 這行定義了當別人寫 from utils import * 時，允許拿走哪些東西
 __all__ = [
