ff77f45c4958da73645b2a8d9f17b6dd5e5ba796
Shang0224 <shangkuo0224@gmail.com>
2026-07-26 11:56:56 +0800

    Update data_fetcher.py

--- utils/data_fetcher.py
+++ utils/data_fetcher.py
@@ -61,7 +61,7 @@ def yf_fetch_all_stocks(stock_ids, start_date, end_date):
             # 延遲避免請求過於頻繁
             time.sleep(2)
 
-            print("-----------{df}----------------")
+            print(f"-----------{df}----------------")
             
         except Exception as e:
             print(f"⚠️ 抓取 {ticker_id} 失敗: {e}")
