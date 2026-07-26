0bfd24b9b2dbb24169dbc39fa0e0fb279d34d750
Shang0224 <shangkuo0224@gmail.com>
2026-07-26 16:31:21 +0800

    Update test_scan.py

--- test_scan.py
+++ test_scan.py
@@ -33,8 +33,8 @@ TEST_END_DATE   = '2025-09-30'  # 設為 None 則只跑單日測試
 STOCK_MODE = 'csv' 
 STOCK_INPUT = os.getenv('STOCK_FILES', 'data/MID100.csv')
 
-STOCK_MODE = 'noncsv' 
-STOCK_INPUT = ['6191', '2377', '2408', '2449', '3044', '2353', '2404']
+#STOCK_MODE = 'noncsv' 
+#STOCK_INPUT = ['6191', '2377', '2408', '2449', '3044', '2353', '2404']
 
 TEST_STRATEGIES = [st_bottom_v_turn]
 #TEST_STRATEGIES = [st_bottom_u_turn]
