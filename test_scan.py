81a172d15c994419482f89c8bd357e850d133ca4
Shang0224 <shangkuo0224@gmail.com>
2026-07-28 09:44:51 +0800

    Update test_scan.py

--- test_scan.py
+++ test_scan.py
@@ -36,7 +36,7 @@ STOCK_INPUT = os.getenv('STOCK_FILES', 'data/MID100.csv')
 #STOCK_MODE = 'noncsv' 
 #STOCK_INPUT = ['6191', '2377', '2408', '2449', '3044', '2353', '2404']
 
-TEST_STRATEGIES = [st_u_bottom]
+TEST_STRATEGIES = [st_bottom_v_turn]
 #TEST_STRATEGIES = [st_bottom_u_turn]
 #TEST_STRATEGIES = [st_shakeout_resurrection]
 #TEST_STRATEGIES = [st_washout_phoenix]
