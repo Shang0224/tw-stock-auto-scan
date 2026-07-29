30495f7e7f857b4785b1c8eb35d899a315f6c3c3
Shang0224 <shangkuo0224@gmail.com>
2026-07-29 12:34:40 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,7 +4,7 @@
 from utils.data_fetcher import yf_fetch_all_stocks, fm_fetch_all_stocks
 from utils.notifier import send_line_message, send_email_with_csv, send_line_summary, send_email_report
 from utils.storage import upload_to_nas
-from utils.file_handler import parse_stock_ids, get_stock_name_dict, send_report, cleanup_local_file, archive_and_cleanup, save_scan_report, save_multi_day_report
+from utils.file_handler import parse_stock_ids, get_stock_name_dict, send_report, cleanup_local_file, archive_and_cleanup, save_scan_report, save_multi_day_report, align_and_normalize_results
 from utils.analytics import calculate_one_year_extremes, calculate_fixed_horizon_returns
 
 
@@ -15,5 +15,5 @@ __all__ = [
     'send_line_message', 'send_email_with_csv', 'send_line_summary', 'send_email_report',
     'upload_to_nas',
     'parse_stock_ids', 'get_stock_name_dict', 'send_report', 'cleanup_local_file', 'archive_and_cleanup', 'save_scan_report', 'save_multi_day_report',
-    'calculate_one_year_extremes', 'calculate_fixed_horizon_returns'
+    'calculate_one_year_extremes', 'calculate_fixed_horizon_returns', 'align_and_normalize_results'
 ]
