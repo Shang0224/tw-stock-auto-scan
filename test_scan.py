7a45791699624645454e866fbbbd1f1ea42ada70
Shang0224 <shangkuo0224@gmail.com>
2026-07-31 17:46:41 +0800

    Update test_scan.py

--- test_scan.py
+++ test_scan.py
@@ -30,7 +30,7 @@ from utils import (
 # =====================================================================
 CHOSEN_SOURCE = 'yf' 
 #CHOSEN_SOURCE = 'fm' 
-TEST_START_DATE = '2024-01-01'
+TEST_START_DATE = '2021-01-01'
 TEST_END_DATE   = '2025-09-30'  # 設為 None 則只跑單日測試
 
 STOCK_MODE = 'csv' 
