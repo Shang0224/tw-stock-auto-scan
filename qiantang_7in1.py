d9263a3631c3508a280f52a428db056a601d7310
Shang0224 <shangkuo0224@gmail.com>
2026-09-12 09:34:28 +0800

    Update qiantang_7in1.py

--- qiantang_7in1.py
+++ qiantang_7in1.py
@@ -80,7 +80,7 @@ def run_seven_combine_filter(stock_list):
             # ==========================================
 
             #處理台股代號後綴
-            if str(stock_id).startswith("^") or "." in str(sid):
+            if str(stock_id).startswith("^") or "." in str(stock_id):
                 yf_id = str(stock_id)
             else:
                 yf_id = f"{stock_id}.TW"
