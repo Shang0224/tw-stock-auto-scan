c7b9f31f7ac97b4c02c31377de2580f32a817953
Shang0224 <shangkuo0224@gmail.com>
2026-09-12 10:13:02 +0800

    Add send_qiantang_7in1_line_summary to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,7 @@
 
 # 把各個子檔案的精華函式拉進來
 from utils.data_fetcher import yf_fetch_all_stocks, fm_fetch_all_stocks, get_fm_trading_days, yf_fetch_monitor_stocks
-from utils.notifier import send_line_message, send_email_with_csv, send_line_summary, send_email_report
+from utils.notifier import send_line_message, send_email_with_csv, send_line_summary, send_email_report, send_qiantang_7in1_line_summary
 from utils.storage import upload_to_nas
 from utils.file_handler import parse_stock_ids, get_stock_name_dict, send_report, cleanup_local_file, archive_and_cleanup, save_scan_report, save_multi_day_report, align_and_normalize_results, process_monitor_stock_data
 from utils.analytics import calculate_one_year_extremes, calculate_fixed_horizon_returns, calculate_forward_horizon_returns
@@ -12,7 +12,7 @@ from utils.analytics import calculate_one_year_extremes, calculate_fixed_horizon
 # 這行定義了當別人寫 from utils import * 時，允許拿走哪些東西
 __all__ = [
     'yf_fetch_all_stocks', 'fm_fetch_all_stocks',
-    'send_line_message', 'send_email_with_csv', 'send_line_summary', 'send_email_report',
+    'send_line_message', 'send_email_with_csv', 'send_line_summary', 'send_email_report', 'send_qiantang_7in1_line_summary'
     'upload_to_nas',
     'parse_stock_ids', 'get_stock_name_dict', 'send_report', 'cleanup_local_file', 'archive_and_cleanup', 'save_scan_report', 'save_multi_day_report',
     'calculate_one_year_extremes', 'calculate_fixed_horizon_returns', 'align_and_normalize_results', 'get_fm_trading_days', 'process_monitor_stock_data', 'yf_fetch_monitor_stocks',
