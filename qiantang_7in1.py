c2d803f9a41f251c3a8fc8b88ac6481f43f59983
Shang0224 <shangkuo0224@gmail.com>
2026-09-12 08:32:45 +0800

    Update qiantang_7in1.py

--- qiantang_7in1.py
+++ qiantang_7in1.py
@@ -273,7 +273,7 @@ def run_seven_combine_filter(stock_list):
     # 🌟 直接呼叫您原本在 utils 裡的 archive_and_cleanup 函數執行 NAS 上傳與清理
     print("📦 [備份啟動] 準備透過 utils 模組將 Excel 報告上傳至 NAS...")
 
-    prod_remote_path = f"{os.getenv('NAS_SFTP_PATH')}/qiantang_7in1/qiantang_7in1_report_{tw_time}.csv"
+    prod_remote_path = f"{os.getenv('NAS_SFTP_PATH')}/qiantang_7in1/qiantang_7in1_report_{datetime.datetime.now()}.csv"
     archive_and_cleanup(file_name, prod_remote_path)
 
 if __name__ == "__main__":
