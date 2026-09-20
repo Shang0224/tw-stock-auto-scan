1103c655eca8edcbebad2822a6d5c9510350f3cb
Shang0224 <shangkuo0224@gmail.com>
2026-09-20 10:39:31 +0800

    Update qiantang_monitor.py

--- monitor/qiantang_monitor.py
+++ monitor/qiantang_monitor.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
 
-import pandas as pd
-import numpy as np
-
+from config import DEBUG_VERBOSE
 
 # ==========================================
 # 錢塘潮 11 大防禦賣訊
